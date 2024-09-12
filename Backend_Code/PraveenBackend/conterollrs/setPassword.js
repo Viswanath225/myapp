0eebd7e1094fce73467be0662648dac738ca3a28
Viswanath225 <165150209+Viswanath225@users.noreply.github.com>
2024-09-12 12:46:09 +0530

    Delete Backend_Code directory

--- Backend_Code/PraveenBackend/conterollrs/setPassword.js
+++
@@ -1,9 +0,0 @@
-const setPassword=async(req,res)=>{
-    const {password,conformPassword}=req.body
-    console.log("Your password is :", req.body);
-    if(password != conformPassword){
-        return res.status(400).json({message:"Passwords do not match"})
-    }
-    res.status(200).send("Password has been set successfully.");
-}
-module.exports={setPassword}
